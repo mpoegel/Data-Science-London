f42c18c33340770e7c49a8eb10b9d8060e37b886
Matt Poegel <mpoegel@comcast.net>
2014-12-17 21:53:33 -0500

    predictions for test data and figures

--- analysis.py
+++ analysis.py
@@ -4,7 +4,6 @@
 #
 # ------------------------------------------------------------------------------
 import numpy as np
-import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
 def PCA(A):
@@ -17,8 +16,9 @@ def PCA(A):
     new_index = np.argsort(eigen_values)[::-1]
     eigen_vectors = eigen_vectors[:,new_index]
     eigen_values = eigen_values[new_index]
+    principal_components = np.dot(eigen_vectors.T, A.T).T
 
-    return np.dot(eigen_vectors.T, A.T).T
+    return eigen_vectors, eigen_values, principal_components
 
 
 def meanClassifier(Cp, Cm):
@@ -30,7 +30,7 @@ def meanClassifier(Cp, Cm):
     meanm = Cm.mean(axis=0)
 
     w = (meanp - meanm)
-    w = w / linalg.norm(w)
+    w = w / np.linalg.norm(w)
 
     t = (np.dot(meanp,w) + np.dot(meanm,w)) / 2
 
@@ -51,16 +51,30 @@ def fisherClassifier(Cp, Cm):
     Bm = Cm - np.ones((msize[0],1)) * meanm
     Sw = np.dot(Bp.T,Bp) + np.dot(Bm.T,Bm)
 
-    w = linalg.solve(Sw, (meanp - meanm).T)
-    w = w / linalg.norm(w)
+    w = np.linalg.solve(Sw, (meanp - meanm).T)
+    w = w / np.linalg.norm(w)
 
     t = (np.dot(meanp,w) + np.dot(meanm,w)) / 2
 
     return (w,t)
 
 
-def classifyData(data, w, t):
-    pass
+def makePredictions(data, w, t, file_name=''):
+    ''' Make predictions for data and write predictions to file
+        Returns the predictions.
+    '''
+    # projections and classifications of the data
+    projections = np.dot(data,w)
+    predictions = np.array([ int(x < t) for x in projections ])
+    # if a file_name is given, then write to that file
+    if (file_name != ''):
+        f = open(file_name, 'w')
+        # print a header to the file
+        f.write("Id,Solution\n")
+        for i in range(len(predictions)):
+            f.write("{0},{1}\n".format(i+1,predictions[i]))
+
+    return predictions
 
 
 def classHist(Cp, Cm, t, title, err):
@@ -81,6 +95,7 @@ def classHist(Cp, Cm, t, title, err):
     return fig
 
 
+# ------------------------------------------------------------------------------
 def main():
     ''' Create models using the Mean and Fisher LDA Methods to predict the
             classes of the test data.
@@ -115,6 +130,9 @@ def main():
            [ int(x >= t) for x in c0_proj ])
     err = (sum(err[0])/len(err[0]) + sum(err[1])/len(err[1])) / 2
     fig1 = classHist(c1_proj, c0_proj, t, "Mean Method", err)
+    # make predictions based off the mean model
+    mean_method_file_name = "MeanMethod_Predictions.txt"
+    makePredictions(testData, w, t, mean_method_file_name)
 
     # calculate the fisher classifier
     (w, t) = fisherClassifier(class1_train, class0_train)
@@ -126,9 +144,12 @@ def main():
            [ int(x >= t) for x in c0_proj ])
     err = (sum(err[0])/len(err[0]) + sum(err[1])/len(err[1])) / 2
     fig2 = classHist(c1_proj, c0_proj, t, "Fisher Method", err)
+    # make predictions based off of the Fisher model
+    fisher_file_name = "Fisher_Predictions.txt"
+    makePredictions(testData, w, t, fisher_file_name)
 
     # Visualize the data by plotting the first two principal components
-    principal_coordinates = PCA(trainData)
+    eigen_vectors, eigen_values, principal_components = PCA(trainData)
     plt.figure(3)
     # break up the principal components by class
     c1_pc = np.array([ trainData[x] for x in range(len(trainData)) \
@@ -141,6 +162,13 @@ def main():
     plt.title("Training Data - First 2 Principal Components")
     plt.xlabel("Principal Component 1")
     plt.ylabel("Principal Component 2")
+    # show the explained variance by each principal component
+    explained_variance = np.cumsum(eigen_values) / sum(eigen_values) * 100
+    plt.figure(4)
+    plt.bar(np.arange(len(explained_variance)), explained_variance)
+    plt.title("Explained Variance of Principal Components")
+    plt.xlabel("Principal Components")
+    plt.ylabel("Percent Explained Explained")
 
 
     plt.show()
